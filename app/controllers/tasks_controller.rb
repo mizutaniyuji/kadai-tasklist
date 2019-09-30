da8a87652ca4b9bf50b6535a797e8d24c05f944d
yuji.mizutani <yuzi0x14@gmail.com>
2019-09-30 13:19:18 +0000

    add pagination and column: status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+     @tasks = Task.all.page(params[:page]).per(10)
   end
 
   def show
@@ -50,6 +50,6 @@ class TasksController < ApplicationController
   
     # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 (1..50).each do |number|
-  Task.create(content: 'task example' + number.to_s)
+  Task.create(content: "task example" + number.to_s, status: number.to_s+"%")
 end
