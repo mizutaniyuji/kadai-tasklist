757300d175755291fe515b6ccb21818f384a7fe5
yuji.mizutani <yuzi0x14@gmail.com>
2019-10-06 09:11:03 +0000

    limit user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :edit]
-  before_action :current_user, only: [:destroy, :show]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :edit, :update, :show]
   
   def index
      @task = current_user.tasks.build  # form_with 用
@@ -13,10 +13,6 @@ class TasksController < ApplicationController
 
   def new
       @task = Task.new
-  end
-  
-  def edit
-    @task = Task.find(params[:id])
   end
 
   def create
@@ -32,11 +28,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
+      
   end
 
   def update
-      @task = Task.find(params[:id])
       if @task.update(task_params)
           flash[:success] = "タスクが正常に更新されました"
           redirect_to(@task)
@@ -47,10 +42,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @task = Task.find(params[:id])
       @task.destroy
       flash[:success] = "タスクは正常に削除されました"
-      redirect_to tasks_url
+      redirect_back(fallback_location: root_path)
   end
   
   
@@ -60,4 +54,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
